75eee1ce51f24a311546d7371c5c67fd0558b2e3
Stefan Gavrilov <stefangavrilov12@gmail.com>
2021-03-29 15:54:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-const port = process.env.Port || 3001;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send("We are on Home")
